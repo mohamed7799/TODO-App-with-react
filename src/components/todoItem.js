60f0a8f6a7d9ac48f31ebfe28c67861669155870
mohmed <mohmedamged9@gmail.com>
2022-10-15 16:19:46 +0200

    remove unnecessary files

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -3,7 +3,7 @@ const TodoItem = () => {
   return (
     <>
       <li className="p-4 flex cursor-pointer items-center group">
-        <span className="bg-no-repeat bg-center group-hover:bg-blue-400 group-hover:bg-check mr-2 w-6 h-6 dark:border-dark_DarkGrayishBlue border-light_LightGrayishBlue border-1 rounded-full inline-block"></span>
+        <span className="bg-no-repeat bg-center group-hover:bg-blue-400 group-hover:bg-check mr-2 w-6 h-6 dark:border-dark_DarkGrayishBlue border-light_LightGrayishBlue border rounded-full inline-block"></span>
         <span className="group-hover:line-through group-hover:text-gray-400">
           item
         </span>
